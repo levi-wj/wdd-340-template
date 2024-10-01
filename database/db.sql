3ab5874b0c46f97d5741c7a1d8299541ddc8a0ef
Levi Jones <leviwill.j@gmail.com>
2024-10-01 17:28:46 -0600

    Added thumbnail to inventory update query

--- database/db.sql
+++ database/db.sql
@@ -53,4 +53,4 @@ VALUES
 
 UPDATE public.inventory SET inv_description=REPLACE(inv_description, 'the small interiors', 'a huge interior') WHERE inv_make='GM' AND inv_model='Hummer';
 
-UPDATE public.inventory SET inv_image=REPLACE(inv_image, '/images/', '/images/vehicles/');
+UPDATE public.inventory SET inv_image=REPLACE(inv_image, '/images/', '/images/vehicles/'), inv_thumbnail=REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
